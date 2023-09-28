3558fc71c4ffa2a94a07a094589bd2ba2452777a
Jake Rawal (k22001853) <jake.rawal3@gmail.com>
2023-09-28 18:10:14 +0100

    Finished Code

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,17 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    length = len(items)
+    for item in items:
+        item = str(item)
+        count = 0
+        if item not in frequencies.keys():
+            for again in items:
+                again = str(again)
+                if again == item:
+                    count += 1
+            frequencies[item] = count
+        
+
     return frequencies
+
